38fd051a843b887e42d49e7656e98c43105b54dd
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-29 18:37:07 +0900

    [issue #10] ファイルの追加

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/main/main.cpp
@@ -0,0 +1,16 @@
+﻿#include "main.h"
+
+#include <windows.h>
+#include <tchar.h>
+#include <crtdbg.h>
+
+#include <cstdio>
+#include <cstdlib>
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	return 0;
+}
+
