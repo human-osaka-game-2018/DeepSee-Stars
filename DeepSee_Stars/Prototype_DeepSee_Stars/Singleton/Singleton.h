1764eadf49176f1bbc6abe74e236e251691bc81b
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-29 18:33:25 +0900

    シングルトンクラスの作成

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/Singleton/Singleton.h
@@ -0,0 +1,30 @@
+﻿#ifndef SINGLETON_H_
+#define SINGLETON_H_
+
+#include <memory>
+
+namespace singleton
+{
+	template <class T>
+	class Singleton
+	{
+	public:
+		Singleton() {};
+		virtual ~Singleton() {};
+
+		/// <summary>
+		/// インスタンスを生成する
+		/// </summary>
+		/// <returns>クラス参照</returns>
+		inline static T& GetInstance()
+		{
+			static std::unique_ptr<T> ptr(new T());
+
+			return *ptr;
+		}
+
+	};
+
+}
+
+#endif // !SINGLETON_H_
