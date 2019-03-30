591f76f9674d0ed0873fc575d174c7568a89db60
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-30 14:25:02 +0900

    [issue #10] クラスをシングルトンで実装

--- DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceManger.h
+++ DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceManger.h
@@ -1,1 +1,17 @@
-﻿#pragma once
+﻿#ifndef DEVICE_MANAGER_H_
+#define DEVICE_MANAGER_H_
+
+#include "../Singleton/Singleton.h"
+
+namespace device
+{
+	class DeviceManager:public singleton::Singleton<DeviceManager>
+	{
+	public:
+		friend class singleton::Singleton<DeviceManager>;
+
+		~DeviceManager() {};
+	};
+}
+
+#endif // !DEVICE_MANAGER_H_
