4e044c5175224255db650939c62f0f58694cbab2
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-31 02:11:42 +0900

    [issue #13] デバイス生成クラスの追加

--- DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceFactory/DeviceFactory.cpp
+++ DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceFactory/DeviceFactory.cpp
@@ -1,1 +1,19 @@
-﻿
+﻿#include "DeviceFactory.h"
+
+namespace device
+{
+	void DeviceFactory::Create(LPDIRECT3D9* ppD3D)
+	{
+		ppD3D = new LPDIRECT3D9();
+	}
+
+	void DeviceFactory::Create(LPDIRECTINPUT8* ppDXInput)
+	{
+		ppDXInput = new LPDIRECTINPUT8();
+	}
+
+	void DeviceFactory::Create(DirectXGraphicDevice* ppDXGraphicDev)
+	{
+		ppDXGraphicDev = new DirectXGraphicDevice();
+	}
+}
--- DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceFactory/DeviceFactory.h
+++ DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceFactory/DeviceFactory.h
@@ -1,5 +1,42 @@
 ﻿#ifndef DEVICE_FACTORY_H_
 #define DEVICE_FACTORY_H_
 
+#include <d3dx9.h>
+#include <dinput.h>
+
+#include "../../DirectXGraphicDevice/DirectXGraphicDevice.h"
+
+namespace device
+{
+	/// <summary>
+	/// デバイス生成クラス
+	/// </summary>
+	class DeviceFactory
+	{
+	public:
+		~DeviceFactory() {}
+
+		/// <summary>
+		/// DirectXのデバイスを生成する
+		/// </summary>
+		/// <param name="ppD3D">DirectXのデバイスのポインタのポインタ</param>
+		static void Create(LPDIRECT3D9* ppD3D);
+
+		/// <summary>
+		/// DirectInputのデバイスを生成する
+		/// </summary>
+		/// <param name="ppDXInput">DInputのデバイスのポインタのポインタ</param>
+		static void Create(LPDIRECTINPUT8* ppDXInput);
+		
+		/// <summary>
+		/// DirectXの描画デバイスを生成する
+		/// </summary>
+		/// <param name="pDXDev">DirectXの描画のデバイスのポインタのポインタ</param>
+		static void Create(DirectXGraphicDevice* pDXGraphicDev);
+
+	private:
+		DeviceFactory() {}
+	};
+}
 
 #endif // !DEVICE_FACTORY_H_
