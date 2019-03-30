c7819a09f1647a26ded9088c010c282a368f316e
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-30 16:04:07 +0900

    [issue #13] メンバ変数とコンストラクタの実装

--- DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceInitializer.h
+++ DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceInitializer.h
@@ -1,4 +1,27 @@
 ﻿#ifndef DEVICE_INITIALIZER_H_
 #define DEVICE_INITIALIZER_H_
 
+#include <windows.h>
+
+#include <d3dx9.h>
+#include <dinput.h>
+
+namespace device
+{
+	class DeviceInitializer
+	{
+	public:
+		DeviceInitializer();
+		~DeviceInitializer() {}
+	
+		static void Initialize();
+
+	private:
+		const HWND hWnd = nullptr;
+		LPDIRECT3D9 m_pD3D = nullptr;
+		LPDIRECTINPUT8 m_pDXInput = nullptr;
+		LPDIRECT3DDEVICE9 m_pDX3DDev = nullptr;
+	};
+}
+
 #endif // !DEVICE_INITIALIZER_H_1
