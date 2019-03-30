47036ff39251b14e1bc1b003d652912336305bcc
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-03-31 02:16:52 +0900

    [issue #13] 初期化しないといけない情報をすべて初期化する関数の実装

--- DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceInitializer.h
+++ DeepSee_Stars/Prototype_DeepSee_Stars/DeviceManger/DeviceInitializer/DeviceInitializer.h
@@ -6,21 +6,57 @@
 #include <d3dx9.h>
 #include <dinput.h>
 
+#include "../DirectXGraphicDevice/DirectXGraphicDevice.h"
+
 namespace device
 {
+	/// <summary>
+	/// デバイス初期化クラス
+	/// </summary>
 	class DeviceInitializer
 	{
 	public:
-		DeviceInitializer();
-		~DeviceInitializer() {}
-	
-		static void Initialize();
+		/// <summary>
+		/// 生成するデバイス情報
+		/// </summary>
+		/// <param name="ppD3D">DirectXのデバイスのポインタのポインタ</param>
+		/// <param name="ppDXInput">DInputのデバイスのポインタのポインタ</param>
+		/// <param name="pDXGraphicDev">DirectXの描画のデバイスのポインタのポインタ</param>
+		DeviceInitializer(LPDIRECT3D9* ppD3D,LPDIRECTINPUT8* ppDXInput, DirectXGraphicDevice* pDXGraphicDev);
+		
+		~DeviceInitializer() 
+		{
+			delete m_pDXGraphicDev;
+			(*m_pDXGraphicDev->m_pDXGraphicDevice).Release();
+			(*m_ppD3D)->Release();
+			(*m_ppDXInput)->Release();
+		}
+		
+		/// <summary>
+		/// デバイスの初期化関数をまとめたもの
+		/// </summary>
+		void InitializeDevices();
 
 	private:
-		const HWND hWnd = nullptr;
-		LPDIRECT3D9 m_pD3D = nullptr;
-		LPDIRECTINPUT8 m_pDXInput = nullptr;
-		LPDIRECT3DDEVICE9 m_pDX3DDev = nullptr;
+		/// <summary>
+		/// DirectXのデバイス情報を初期化する
+		/// </summary>
+		void InitializeDXDevice();
+
+		/// <summary>
+		/// 入力デバイス情報を初期化する
+		/// </summary>
+		void InitializeInputDevice();
+
+		/// <summary>
+		/// 描画のデバイス情報を初期化する
+		/// </summary>
+		void InitializeGraphicDevice();
+
+		const HWND& hWnd;
+		LPDIRECT3D9* m_ppD3D;
+		LPDIRECTINPUT8* m_ppDXInput;
+		DirectXGraphicDevice* m_pDXGraphicDev;
 	};
 }
 
