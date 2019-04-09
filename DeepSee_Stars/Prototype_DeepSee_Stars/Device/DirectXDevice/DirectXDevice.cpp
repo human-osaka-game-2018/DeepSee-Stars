3275c0b7714db29a4a200f335bf9cdd00e65aa85
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-04-09 15:11:14 +0900

    [issue #13] DirectXのデバイスを隠蔽したクラスの実装

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/Device/DirectXDevice/DirectXDevice.cpp
@@ -0,0 +1,18 @@
+﻿#include "DirectXDevice.h"
+
+namespace device
+{
+	void DirectXDevice::Create()
+	{
+		if (m_pDIRECT3D9 = Direct3DCreate9(D3D_SDK_VERSION))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("Direct3Dの作成に失敗しました"),
+			NULL,
+			MB_OK);
+	}
+}
