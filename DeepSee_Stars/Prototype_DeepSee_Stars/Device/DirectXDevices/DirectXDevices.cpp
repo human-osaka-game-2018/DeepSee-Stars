80d402b5822323342e4f696821f60f7701b17e6d
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-04-09 15:43:13 +0900

    [issue #13] DirectXのデバイスをまとめたクラスの実装

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/Device/DirectXDevices/DirectXDevices.cpp
@@ -0,0 +1,8 @@
+﻿#include "DirectXDevices.h"
+
+namespace device
+{
+	DirectXDevice*			DirectXDevices::m_pDXDev = nullptr;
+	DirectXGraphicDevice*	DirectXDevices::m_pDXGraphicDev = nullptr;
+	DirectXInputDevice*		DirectXDevices::m_pDXInput = nullptr;
+}
