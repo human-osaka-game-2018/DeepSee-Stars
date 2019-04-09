b7336bc3c3f313e00ee56fe495931094afce489b
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-04-09 15:15:07 +0900

    [issue #13] DirectXの入力デバイスを隠蔽したクラスの実装

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/Device/DirectXInputDevice/DirectXInputDevice.cpp
@@ -0,0 +1,14 @@
+﻿#include "DirectXInputDevice.h"
+
+namespace device
+{
+	void DirectXInputDevice::Create()
+	{
+		DirectInput8Create(
+			GetModuleHandle(NULL),
+			DIRECTINPUT_VERSION, IID_IDirectInput8,
+			(void**)&m_DXInput,
+			NULL);
+	}
+
+}
