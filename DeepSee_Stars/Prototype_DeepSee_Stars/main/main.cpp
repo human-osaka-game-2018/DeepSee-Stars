da34146919007492e440d8288907fa9bcfd2eb39
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-04-09 16:16:48 +0900

    [issue #13] main関数で呼ぶ関数の追加

--- DeepSee_Stars/Prototype_DeepSee_Stars/main/main.cpp
+++ DeepSee_Stars/Prototype_DeepSee_Stars/main/main.cpp
@@ -1,15 +1,25 @@
-﻿#include "main.h"
-
-#include <windows.h>
+﻿#include <windows.h>
 #include <tchar.h>
 #include <crtdbg.h>
 
 #include <cstdio>
 #include <cstdlib>
 
+#include "../Wnd/Wnd.h"
+#include "../Device/DirectXDevices/DirectXDevices.h"
+
+using namespace window;
+using namespace device;
+
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	Wnd::Create(hInst, _T("DeepSee_Stars"));
+
+	DirectXDevices::Create();
+
+	DirectXDevices::Release();
 
 	return 0;
 }
