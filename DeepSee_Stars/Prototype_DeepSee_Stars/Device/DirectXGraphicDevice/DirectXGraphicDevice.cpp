2a79b942c61e48a47452ee415e8209f7eae93caf
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-04-09 15:12:17 +0900

    [issue #13] DirectXの描画関連デバイスを隠蔽したクラスの実装

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/Device/DirectXGraphicDevice/DirectXGraphicDevice.cpp
@@ -0,0 +1,47 @@
+﻿#include "DirectXGraphicDevice.h"
+
+namespace device
+{
+	void DirectXGraphicDevice::Create(DirectXDevice* pDirectXDevice)
+	{
+		m_pD3Dpp = new D3DPP(window::Wnd::GetHWND(), window::Wnd::GetWndSize());
+
+		HWND hWnd = window::Wnd::GetHWND();
+		LPDIRECT3D9 DXDevice = pDirectXDevice->GetDXDevice();
+		D3DPRESENT_PARAMETERS presentParameters = m_pD3Dpp->GetD3DPRESENT_PARAMETERS();
+
+		if (SUCCEEDED(DXDevice->CreateDevice(	//描画をハードウェアに依存させる 軽い
+			D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL,
+			hWnd,
+			D3DCREATE_HARDWARE_VERTEXPROCESSING | D3DCREATE_MULTITHREADED,
+			&presentParameters,
+			&m_pDXGraphicDevice)))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("HALモードでDIRECT3Dデバイスを作成できません/nREFモードで再試行します"),
+			NULL,
+			MB_OK);
+
+		if (SUCCEEDED(DXDevice->CreateDevice(	//描画をソフトウェアに依存させる 重い
+			D3DADAPTER_DEFAULT, D3DDEVTYPE_REF,
+			hWnd,
+			D3DCREATE_MIXED_VERTEXPROCESSING | D3DCREATE_MULTITHREADED,
+			&presentParameters,
+			&m_pDXGraphicDevice)))
+		{
+			return;
+		}
+
+		MessageBox(
+			0,
+			_T("DIRECT3Dデバイスの作成に失敗しました"),
+			NULL,
+			MB_OK);
+
+		DestroyWindow(hWnd);	//WM_DESTROYをWndProcに投げる
+	}
+}
