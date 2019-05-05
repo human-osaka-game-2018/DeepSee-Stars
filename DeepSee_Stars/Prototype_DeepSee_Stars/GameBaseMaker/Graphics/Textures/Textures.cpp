cebb40c46da9b27458cd760aa695039fa98a6be6
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-05 17:40:09 +0900

    テクスチャを管理するクラスの追加

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/GameBaseMaker/Graphics/Textures/Textures.cpp
@@ -0,0 +1,36 @@
+﻿#include "Textures.h"
+
+namespace graphics
+{
+	void Textures::CreateTex(const TCHAR* pKey, const TCHAR* pTexPath)
+	{
+		if (Exists(pKey)) return;
+
+		m_textures[pKey].CreateTex(pKey, pTexPath);
+	}
+
+	void Textures::EraseTex(const TCHAR* pKey)
+	{
+		if (!Exists(pKey)) return;
+
+		m_textures[pKey].Release();
+		m_textures.erase(pKey);
+	}
+
+	void Textures::ReleaseAllTex()
+	{
+		if (!m_textures.size()) return;
+
+		for(auto texture : m_textures)
+		{
+			(texture.second).Release();
+		}
+
+		m_textures.clear();
+	}
+
+	bool Textures::Exists(const TCHAR* pKey)
+	{
+		return (m_textures.find(pKey) != m_textures.end());
+	}
+}
