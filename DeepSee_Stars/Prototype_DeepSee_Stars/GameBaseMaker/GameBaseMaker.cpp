bf4985fcac679156440326cd8fda37c70a29661a
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-05 23:52:25 +0900

    入力と描画するために必要な情報をまとめたクラスの追加

---
+++ DeepSee_Stars/Prototype_DeepSee_Stars/GameBaseMaker/GameBaseMaker.cpp
@@ -0,0 +1,34 @@
+﻿#include "GameBaseMaker.h"
+
+#include <chrono>
+#include "../Wnd/Wnd.h"
+
+namespace gamebasemaker
+{
+	using window::Wnd;
+
+	void GameBaseMaker::Run(std::function<void()> Func)
+	{
+		std::chrono::system_clock::time_point currentTime, prevTime;
+		const INT DEFAULT_FPS = 60;
+
+		while (!Wnd::IsPostedQuitMessage())
+		{
+			if (Wnd::ExistsWinMSG()) continue;
+
+			currentTime = std::chrono::system_clock::now();
+
+			LONGLONG rapTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - prevTime).count();
+
+			if (rapTime < 1000 / DEFAULT_FPS) continue;
+
+			prevTime = currentTime;
+
+			Prepare();
+
+			Func();
+
+			CleanUp();
+		}
+	}
+}
