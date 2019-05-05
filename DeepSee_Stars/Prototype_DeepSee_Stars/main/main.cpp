d2c25873d0f4f903489c0bfc7e83b495efd1c4b1
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-05 23:53:05 +0900

    テストプログラムコードの追加

--- DeepSee_Stars/Prototype_DeepSee_Stars/main/main.cpp
+++ DeepSee_Stars/Prototype_DeepSee_Stars/main/main.cpp
@@ -8,8 +8,48 @@
 #include "../Wnd/Wnd.h"
 #include "../Device/DirectXDevices/DirectXDevices.h"
 
+#include "../GameBaseMaker/GameBaseMaker.h"
+
 using namespace window;
 using namespace device;
+using namespace gamebasemaker;
+
+class test:public singleton::Singleton<test>
+{
+public:
+	friend class singleton::Singleton<test>;
+
+	test()
+	{
+		TextureUV uv(D3DXVECTOR2(0.f, 0.f),
+			D3DXVECTOR2(2048.f, 2048.f), D3DXVECTOR2(250.f, 300.f));
+		verticies = new Vertices(uv);
+	}
+
+	void Update()
+	{	
+		rGameBaseMaker.CreateTex(_T("back"), _T("2Dtexture/Sheep_Stand-by.png"));	
+	}
+
+	void Render()
+	{
+		D3DXVECTOR2 pos = verticies->GetDisplaySize();
+		verticies->SetPos(D3DXVECTOR3(pos.x/2.f, pos.y/2.f, 0.0f));
+		verticies->Animation(30, 4);
+		rGameBaseMaker.Render(*verticies, rGameBaseMaker.GetTex(_T("back")));
+	}
+
+private:
+	GameBaseMaker& rGameBaseMaker = gamebasemaker::GameBaseMaker::GetInstance();
+	Vertices* verticies = nullptr;
+};
+
+void RunMain()
+{
+	test& ts = test::GetInstance();
+	ts.Update();
+	ts.Render();
+}
 
 INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
 {
@@ -18,6 +58,10 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdSh
 	Wnd::Create(hInst, _T("DeepSee_Stars"));
 
 	DirectXDevices::Create();
+
+	gamebasemaker::GameBaseMaker& rGameBaseMaker = gamebasemaker::GameBaseMaker::GetInstance();
+
+	rGameBaseMaker.Run(RunMain);
 
 	DirectXDevices::Release();
 
