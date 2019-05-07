0329bace166c7f3dff3954eb5d13872fcc87ce34
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-07 16:50:07 +0900

    テクスチャクラスをまとめたクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Graphics/Textures/Textures.h
@@ -0,0 +1,55 @@
+﻿#ifndef TEXTURES_H_
+#define TEXTURES_H_
+
+#include <tchar.h>
+#include <unordered_map>
+
+#include "Texture/Texture.h"
+
+namespace graphics
+{
+	/// <summary>
+	/// テクスチャの情報をまとめたクラス
+	/// </summary>
+	class Textures
+	{
+	public:
+		Textures() {}
+		~Textures() {}
+
+		/// <summary>
+		/// テクスチャの生成
+		/// </summary>
+		/// <param name="pKey">テクスチャのキーワード</param>
+		/// <param name="pTexPath">テクスチャのファイルパス</param>
+		void CreateTex(const TCHAR* pKey, const TCHAR* pTexPath);
+
+		/// <summary>
+		/// 指定したテクスチャの削除
+		/// </summary>
+		/// <param name="pKey">削除したいテクスチャのキーワード</param>
+		void EraseTex(const TCHAR* pKey);
+
+		/// <summary>
+		/// テクスチャの全開放
+		/// </summary>
+		void ReleaseAllTex();
+
+		inline Texture GetTex(const TCHAR* pKey)
+		{
+			return m_textures[pKey];
+		}
+
+	private:
+		/// <summary>
+		/// テクスチャが存在するか
+		/// </summary>
+		/// <param name="pKey">テクスチャのキーワード</param>
+		/// <returns>存在していたらtrue</returns>
+		bool Exists(const TCHAR* pKey);
+
+		std::unordered_map<const TCHAR*, Texture> m_textures;
+	};
+}
+
+#endif // !TEXTURES_H_
