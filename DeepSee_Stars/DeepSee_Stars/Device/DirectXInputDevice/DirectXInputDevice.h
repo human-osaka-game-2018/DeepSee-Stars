663931710b906496b4da9d93b0d4a7a7253c053f
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-07 16:46:40 +0900

    DirectXの入力デバイスの隠蔽クラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/Device/DirectXInputDevice/DirectXInputDevice.h
@@ -0,0 +1,43 @@
+﻿#ifndef DIRECTX_INPUT_DEVICE_H_
+#define DIRECTX_INPUT_DEVICE_H_
+
+#include <dinput.h>
+
+namespace device
+{
+	/// <summary>
+	/// DirectXInputデバイスをまとめたクラス
+	/// </summary>
+	class DirectXInputDevice
+	{
+	public:
+		DirectXInputDevice()
+		{
+			Create();
+		}
+
+		~DirectXInputDevice()
+		{
+			m_DXInput->Release();
+		}
+
+		/// <summary>
+		/// 入力デバイスのゲッター
+		/// </summary>
+		/// <returns>デバイスのポインタ</returns>
+		inline LPDIRECTINPUT8 Get() const
+		{
+			return m_DXInput;
+		}
+
+	private:
+		/// <summary>
+		/// デバイスの生成
+		/// </summary>
+		void Create();
+
+		LPDIRECTINPUT8 m_DXInput = nullptr;
+	};
+}
+
+#endif // !DIRECTX_INPUT_DEVICE_H_
