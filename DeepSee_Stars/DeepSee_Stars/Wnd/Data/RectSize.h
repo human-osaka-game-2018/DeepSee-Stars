46f63805d2029993a53b315688a38415a63bb4ea
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-07 16:42:14 +0900

    プロトタイプの移植版wndクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/Wnd/Data/RectSize.h
@@ -0,0 +1,18 @@
+﻿#ifndef RECT_SIZE_H_
+#define RECT_SIZE_H_
+
+#include <windows.h>
+
+#include <d3dx9.h>
+
+/// <summary>
+///  平面上で用いられるサイズなどの値
+/// </summary>
+struct RectSize
+{
+public:
+	int m_x = 0;
+	int m_y = 0;
+};
+
+#endif // !RECT_SIZE_H_
---
+++ DeepSee_Stars/DeepSee_Stars/Wnd/Wnd.h
@@ -0,0 +1,90 @@
+﻿#ifndef WND_H
+#define WND_H
+
+#include <windows.h>
+
+#include "Data/RectSize.h"
+
+namespace window
+{
+	/// <summary>
+	/// ウィンドウクラス
+	/// </summary>
+	class Wnd
+	{
+	public:
+		/// <summary>
+		/// ウィンドウを生成する
+		/// </summary>
+		/// <param name="hInst">hInstance</param>
+		/// <param name="pAppName">アプリ名</param>
+		static void Create(const HINSTANCE hInst, const TCHAR* pAppName);
+
+		/// <summary>
+		/// ウィンドウハンドルを取得する
+		/// </summary>
+		/// <returns>ウィンドウハンドル</returns>
+		static inline const HWND GetHWND()
+		{
+			return m_hWnd;
+		}
+
+		/// <summary>
+		/// WinMessageの構造体を取得する
+		/// </summary>
+		/// <returns>WinMessageの構造体</returns>
+		static inline MSG GetMSG()
+		{
+			return m_msg;
+		}
+
+		/// <summary>
+		/// ウィンドウのサイズを取得する
+		/// </summary>
+		/// <returns>ウィンドウのサイズ</returns>
+		static inline RectSize GetWndSize()
+		{
+			return WND_SIZE;
+		}
+
+		/// <summary>
+		/// ウィンドウを終了するメッセージが投げられたか
+		/// </summary>
+		/// <returns>投げられていたらTRUE</returns>
+		static inline bool IsPostedQuitMessage()
+		{
+			if (m_msg.message == WM_QUIT) return TRUE;
+
+			return FALSE;
+		}
+
+		/// <summary>
+		/// WinMessageが何かあるか
+		/// </summary>
+		/// <returns>あればTRUE</returns>
+		static bool ExistsWinMSG();
+
+		/// <summary>
+		/// クライアント領域をm_WND_SIZEと同じにする
+		/// </summary>
+		static void ResizeWnd();
+
+	private:
+		Wnd() {}
+		~Wnd() {}
+
+		/// <summary>
+		/// ウィンドウを生成するときにProcの関数ポインタが必要なのでstatic
+		/// IDX3D経由でDX3DのメソッドToggleWndMode()を呼ぶ
+		/// </summary>
+		static LRESULT CALLBACK WndProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam);    
+																								
+		static HWND m_hWnd;
+
+		static MSG m_msg;
+
+		static const RectSize WND_SIZE;
+	};
+}
+
+#endif // !WND_H
