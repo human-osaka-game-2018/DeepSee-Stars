a83e505f385538fa1870abcab54ca75cbfdf79f1
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-07 16:45:41 +0900

    バックバッファの設定クラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/Device/DirectXGraphicDevice/PresentParam/PresentParam.cpp
@@ -0,0 +1,23 @@
+﻿#include "PresentParam.h"
+
+namespace device
+{
+	void PresentParam::InitPresentParam(D3DPRESENT_PARAMETERS* pD3DPP)
+	{
+		ZeroMemory(pD3DPP, sizeof(D3DPRESENT_PARAMETERS));
+
+		pD3DPP->Windowed = m_isWindow;
+		pD3DPP->BackBufferFormat = D3DFMT_X8R8G8B8;
+		pD3DPP->BackBufferCount = 1;
+		pD3DPP->SwapEffect = D3DSWAPEFFECT_DISCARD;
+		pD3DPP->EnableAutoDepthStencil = TRUE;
+		pD3DPP->AutoDepthStencilFormat = D3DFMT_D16;
+		pD3DPP->BackBufferWidth = WND_SIZE.m_x;
+		pD3DPP->BackBufferHeight = WND_SIZE.m_y;
+
+		if (m_isWindow) return;
+
+		pD3DPP->hDeviceWindow = hWND;
+		pD3DPP->PresentationInterval = D3DPRESENT_INTERVAL_DEFAULT;
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/Device/DirectXGraphicDevice/PresentParam/PresentParam.h
@@ -0,0 +1,60 @@
+﻿#ifndef PRESENT_PARAM_H_
+#define PRESENT_PARAM_H_
+
+#include <windows.h>
+
+#include "../../../Wnd/Data/RectSize.h"
+
+namespace device
+{
+	/// <summary>
+	/// バックバッファ関係クラス
+	/// </summary>
+	class PresentParam
+	{
+	public:
+		PresentParam(HWND hWnd, RectSize wndSize) :WND_SIZE(wndSize), hWND(hWnd) {};
+		~PresentParam() {};
+
+		/// <summary>
+		/// 現在のウィンドウモードのD3DPRESENT_PARAMETERSを返す
+		/// </summary>
+		/// <returns>現在のウィンドウモードのD3DPRESENT_PARAMETERS </returns>
+		inline D3DPRESENT_PARAMETERS GetD3DPRESENT_PARAMETERS()
+		{
+			D3DPRESENT_PARAMETERS PresentParam;
+			InitPresentParam(&PresentParam);
+
+			return PresentParam;
+		}
+
+		/// <summary>
+		/// ウィンドウモードを切り替えそのD3DPRESENT_PARAMETERSを返す
+		/// </summary>
+		/// <returns>ウィンドウモード切替後のD3DPRESENT_PARAMETERS</returns>
+		inline D3DPRESENT_PARAMETERS ToggleD3DPPWndMode()
+		{
+			m_isWindow = !m_isWindow;
+
+			D3DPRESENT_PARAMETERS PresentParam;
+			InitPresentParam(&PresentParam);
+
+			return PresentParam;
+		}
+
+	private:
+		/// <summary>
+		/// D3DPRESENT_PARAMETERSを初期化する
+		/// </summary>
+		/// <param name="pD3DPP">初期化したいD3DPRESENT_PARAMETERS型のポインタ</param>
+		void InitPresentParam(D3DPRESENT_PARAMETERS* pPresentParam);
+
+		const HWND hWND;
+
+		const RectSize WND_SIZE;
+
+		bool m_isWindow = true;
+	};
+}
+
+#endif // !PRESENT_PARAM_H_
