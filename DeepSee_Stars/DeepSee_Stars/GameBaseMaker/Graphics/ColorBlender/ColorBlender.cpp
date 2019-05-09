c2ffbc7fb1c45fb8ee157f69ff0c0270b5eb51a2
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 13:19:46 +0900

    色の設定関連クラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Graphics/ColorBlender/ColorBlender.cpp
@@ -0,0 +1,20 @@
+﻿#include "ColorBlender.h"
+
+namespace graphics
+{
+	void ColorBlender::DefaultColorBlending() const
+	{
+		DefaultBlendMode();
+		m_DXGraphicDevice->GetGraphicDevice()->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+		m_DXGraphicDevice->GetGraphicDevice()->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+		m_DXGraphicDevice->GetGraphicDevice()->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
+		m_DXGraphicDevice->GetGraphicDevice()->SetRenderState(D3DRS_ALPHAFUNC, D3DCMP_GREATEREQUAL);
+		m_DXGraphicDevice->GetGraphicDevice()->SetRenderState(D3DRS_ALPHAREF, 0x01);
+
+		m_DXGraphicDevice->GetGraphicDevice()->SetTextureStageState(0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE);
+		m_DXGraphicDevice->GetGraphicDevice()->SetTextureStageState(0, D3DTSS_ALPHAOP, D3DTOP_MODULATE);
+		
+		m_DXGraphicDevice->GetGraphicDevice()->SetTextureStageState(0, D3DTSS_COLORARG1, D3DTA_TEXTURE);
+		m_DXGraphicDevice->GetGraphicDevice()->SetTextureStageState(0, D3DTSS_COLOROP, D3DTOP_MODULATE);
+	}
+}
