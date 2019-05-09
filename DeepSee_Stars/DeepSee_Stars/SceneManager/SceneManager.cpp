3f7cb501eaef7bae897446c5a2632baaced63f8a
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 19:17:40 +0900

    シーン管理クラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/SceneManager.cpp
@@ -0,0 +1,32 @@
+﻿#include "SceneManager.h"
+
+namespace deepseestars
+{
+	void SceneManager::Factory()
+	{
+		if (m_currentScene == m_nextScene) return;
+
+		m_currentScene = m_nextScene;
+
+		switch (m_nextScene)
+		{
+		case SCENE_ID::TITLE:
+			delete m_pScene;
+
+			break;
+
+		case SCENE_ID::GAME:
+			delete m_pScene;
+
+			break;
+
+		case SCENE_ID::RESULT:
+			delete m_pScene;
+
+			break;
+
+		default:
+			break;
+		}
+	}
+}
