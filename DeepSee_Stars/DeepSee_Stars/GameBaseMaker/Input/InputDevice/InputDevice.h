d5ecdf521ca3c07c51e09fe246e48f589f58db93
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 13:26:56 +0900

    DirectXの入力デバイスを隠蔽したクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Input/InputDevice/InputDevice.h
@@ -0,0 +1,74 @@
+﻿#ifndef INPUT_DEVICE_H_
+#define INPUT_DEVICE_H_
+
+#include <Windows.h>
+
+#include <dinput.h>
+
+#include "../../../Wnd/Wnd.h"
+
+namespace input
+{
+	/// <summary>
+	/// 入力デバイス関係の基底クラス
+	/// </summary>
+	class InputDevice
+	{
+	public:
+		InputDevice() :m_HWND(window::Wnd::GetHWND()) {}
+		virtual ~InputDevice() {}
+
+		/// <summary>
+		/// ボタンが押された瞬間かを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>押された瞬間true</returns>
+		virtual bool IsPressed(int key) = 0;
+
+		/// <summary>
+		/// ボタンが押されているかを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>押されていたらtrue</returns>
+		virtual bool IsHold(int key) = 0;
+
+		/// <summary>
+		/// ボタンが離された瞬間かを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>離された瞬間true</returns>
+		virtual bool IsReleased(int key) = 0;
+		
+		/// <summary>
+		/// ボタンが離されているかを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>離されていたらtrue</returns>
+		virtual bool IsNeutral(int key) = 0;
+
+	protected:
+		enum INPUT_DETAIL
+		{
+			IND_PRESS,										//押された瞬間のフレームだけTRUE
+			IND_HOLD,										//押され続けられている間TRUE
+			IND_RELEASE,									//離された瞬間のフレームだけTRUE
+			IND_NEUTRAL,									//前回と現在のフレームで押されていない間TRUE
+			IND_MAX
+		};
+
+		virtual void AcquireInputState() = 0;
+		virtual void CheckInputStateDetatils() = 0;
+		virtual void Create() = 0;
+
+		inline bool IsInputted(const BYTE& rKey) const
+		{
+			return (rKey & 0x80);
+		}
+
+		const HWND m_HWND = nullptr;
+
+		LPDIRECTINPUTDEVICE8 m_pDXInputDevice = nullptr;
+	};
+}
+
+#endif // !INPUT_DEVICE_H_
