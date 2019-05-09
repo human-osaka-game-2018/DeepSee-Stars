5a1c26648b320b4b4aab682b2dac8c09439e6f60
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 19:23:22 +0900

    ゲームオブジェクトのファーストクラスコレクションクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameObjects/GameObjects.h
@@ -0,0 +1,50 @@
+﻿#ifndef GAME_OBJECTS_H_
+#define GAME_OBJECTS_H_
+
+#include <vector>
+
+#include "GameObject/GameObject.h"
+
+namespace deepseestars
+{
+	class GameObjects
+	{
+	public:
+		GameObjects() {}
+		~GameObjects() {}
+		
+		/// <summary>
+		/// ゲームオブジェクトを登録する
+		/// </summary>
+		/// <param name="gameObject">登録したいゲームオブジェクトのポインタ</param>
+		inline void Register(GameObject* gameObject)
+		{
+			m_gameObjects.push_back(gameObject);
+		}
+
+		/// <summary>
+		/// ゲームオブジェクトの中身をすべて初期化する
+		/// </summary>
+		void Init();
+
+		/// <summary>
+		/// ゲームオブジェクトの中身をすべて更新する
+		/// </summary>
+		void Update();
+
+		/// <summary>
+		/// ゲームオブジェクトの中身をすべて描画する
+		/// </summary>
+		void Render();
+
+		/// <summary>
+		/// ゲームオブジェクトの中身をすべて解放する
+		/// </summary>
+		void Release();
+
+	private:
+		std::vector<GameObject*> m_gameObjects;
+	};
+}
+
+#endif // !GAME_OBJECTS_H_
