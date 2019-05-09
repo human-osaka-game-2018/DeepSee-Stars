cd0675473835fa86751c46f95addbd9e3ec649c8
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 19:23:22 +0900

    ゲームオブジェクトのファーストクラスコレクションクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameObjects/GameObjects.cpp
@@ -0,0 +1,36 @@
+﻿#include "GameObjects.h"
+
+namespace deepseestars
+{
+	void GameObjects::Init()
+	{
+		for (auto* object : m_gameObjects)
+		{
+			object->Init();
+		}
+	}
+
+	void GameObjects::Update()
+	{
+		for (auto* object : m_gameObjects)
+		{
+			object->Update();
+		}
+	}
+
+	void GameObjects::Render()
+	{
+		for (auto* object : m_gameObjects)
+		{
+			object->Render();
+		}
+	}
+
+	void GameObjects::Release()
+	{
+		for (auto* object : m_gameObjects)
+		{
+			object->Release();
+		}
+	}
+}
