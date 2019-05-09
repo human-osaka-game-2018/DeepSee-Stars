2c967a16eb34c71d74c2e13733f5e90957817ce0
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 13:51:53 +0900

    メインファイルの追加

---
+++ DeepSee_Stars/DeepSee_Stars/main/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <crtdbg.h>
+
+#include <cstdio>
+#include <cstdlib>
+
+#include "../Wnd/Wnd.h"
+#include "../Device/DirectXDevices/DirectXDevices.h"
+
+#include "../GameBaseMaker/GameBaseMaker.h"
+
+using namespace window;
+using namespace device;
+using namespace gamebasemaker;
+
+void RunMain()
+{
+	
+}
+
+INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR szStr, INT iCmdShow)
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	Wnd::Create(hInst, _T("DeepSee_Stars"));
+
+	DirectXDevices::Create();
+
+	gamebasemaker::GameBaseMaker& rGameBaseMaker = gamebasemaker::GameBaseMaker::GetInstance();
+
+	rGameBaseMaker.Run(RunMain);
+
+	DirectXDevices::Release();
+
+	return 0;
+}
+
