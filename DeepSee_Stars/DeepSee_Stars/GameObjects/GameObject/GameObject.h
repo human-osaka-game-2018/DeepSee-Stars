b2bead8839d682479f84aacf8d9fba3905110c94
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 19:23:02 +0900

    ゲームオブジェクトの基底クラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameObjects/GameObject/GameObject.h
@@ -0,0 +1,44 @@
+﻿#ifndef GAME_OBJECT_H_
+#define GAME_OBJECT_H_
+
+#include "../../GameBaseMaker/GameBaseMaker.h"
+
+namespace deepseestars
+{
+	using gamebasemaker::GameBaseMaker;
+
+	/// <summary>
+	/// ゲームのオブジェクト基底クラス
+	/// </summary>
+	class GameObject
+	{
+	public:
+		GameObject() {}
+		virtual ~GameObject() {}
+
+		/// <summary>
+		/// 基底初期化関数
+		/// </summary>
+		virtual void Init() = 0;
+
+		/// <summary>
+		/// 基底更新関数
+		/// </summary>
+		virtual void Update() = 0;
+
+		/// <summary>
+		/// 基底描画関数
+		/// </summary>
+		virtual void Render() = 0;
+
+		/// <summary>
+		/// 基底解放関数
+		/// </summary>
+		virtual void Release() = 0;
+
+	private:
+		GameBaseMaker& rGameBaseMaker = GameBaseMaker::GetInstance();
+	};
+}
+
+#endif // !GAME_OBJECT_H_
