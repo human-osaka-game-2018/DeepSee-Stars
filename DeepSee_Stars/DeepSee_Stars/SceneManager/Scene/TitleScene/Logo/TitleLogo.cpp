3d7a6609ed752936acb183ed3febb1a546b321e4
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-10 02:04:25 +0900

    タイトルロゴクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/TitleScene/Logo/TitleLogo.cpp
@@ -0,0 +1,20 @@
+﻿#include "TitleLogo.h"
+
+namespace deepseestars
+{
+	void TitleLogo::Render()
+	{
+		D3DXVECTOR2 pos = 
+		{ m_vertices.GetDisplaySize().x * 0.5f ,m_vertices.GetDisplaySize().y * 0.25f };
+		D3DXVECTOR2 scale = m_vertices.GetDisplaySize() * 0.25f;
+		
+		m_vertices.SetPos(pos);
+		m_vertices.SetScale(scale);
+
+		const int FADEIN_FRAME = 180;
+
+		m_vertices.FadeIn(0xff, 0x00, FADEIN_FRAME);
+
+		m_rGameBaseMaker.Render(m_vertices, m_rGameBaseMaker.GetTex(m_pTextureKey));
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/TitleScene/Logo/TitleLogo.h
@@ -0,0 +1,39 @@
+﻿#ifndef TITLE_LOGO_H_
+#define TITLE_LOGO_H_
+
+#include "GameObject.h"
+
+namespace deepseestars
+{
+	class TitleLogo :public GameObject
+	{
+	public:
+		TitleLogo() :
+			m_pFileName(_T("2DTexture/Title/TitleLogo.png")), m_pTextureKey(_T("Logo"))
+		{
+			Init();
+		}
+
+		~TitleLogo() {}
+			
+		void Init()
+		{
+			m_rGameBaseMaker.CreateTex(m_pTextureKey, m_pFileName);
+		}
+
+		void Update() {}
+
+		void Render();
+
+		void Release()
+		{
+			m_rGameBaseMaker.ReleaseAllTex();
+		}
+
+	private:
+		const TCHAR* m_pFileName;
+		const TCHAR* m_pTextureKey;
+	};
+}
+
+#endif // !TITLE_LOGO_H_
