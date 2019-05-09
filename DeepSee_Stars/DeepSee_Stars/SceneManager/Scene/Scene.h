a7786b1eb3d86422df03545610462fe1438abbc7
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 19:17:18 +0900

    シーン基底クラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/Scene.h
@@ -0,0 +1,61 @@
+﻿#ifndef SCENE_H_
+#define SCENE_H_
+
+#include "../../GameObjects/GameObjects.h"
+
+namespace deepseestars
+{
+	/// <summary>
+	/// シーン基底クラス
+	/// </summary>
+	class Scene
+	{
+	public:
+		Scene()
+		{
+			Init();
+		}
+
+		virtual ~Scene()
+		{
+			Release();
+		}
+
+		/// <summary>
+		/// シーンで使うオブジェクトを初期化する
+		/// </summary>
+		virtual void Init()
+		{
+			m_objects.Init();
+		}
+
+		/// <summary>
+		/// シーンで使うオブジェクトを更新する 
+		/// </summary>
+		virtual void Update()
+		{
+			m_objects.Update();
+		}
+
+		/// <summary>
+		/// シーンで使うオブジェクトを描画する
+		/// </summary>
+		virtual void Render()
+		{
+			m_objects.Render();
+		}
+
+		/// <summary>
+		/// シーンで使うオブジェクトを解放する
+		/// </summary>
+		virtual void Release()
+		{
+			m_objects.Release();
+		}
+
+	private:
+		GameObjects m_objects;
+	};
+}
+
+#endif // !SCENE_H_
