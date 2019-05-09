3d3b0fe3dd10579982d6b792d5319b02f95c0fb5
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 13:04:50 +0900

    画像を描画するクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Graphics/Renderer/Renderer.h
@@ -0,0 +1,44 @@
+﻿#ifndef RENDERER_H_
+#define RENDERER_H_
+
+#include <tchar.h>
+
+#include <d3dx9.h>
+
+#include "../../../Device/DirectXDevices/DirectXDevices.h"
+#include "../../Vertices/Vertices.h"
+#include "../Textures/Textures.h"
+
+namespace graphics
+{
+	class Renderer
+	{
+	public:
+		Renderer();
+		~Renderer() {}
+
+		/// <summary>
+		/// 描画準備をする
+		/// </summary>
+		/// <remarks>メッセージループの最初で呼ぶ必要がある</remarks>
+		void PrepareRendering();
+
+		/// <summary>
+		/// 描画を終了させる
+		/// </summary>
+		/// <remarks>メッセージループの最後に呼ぶ必要がある</remarks>
+		void CleanUpRendering();
+
+		/// <summary>
+		/// 画像を描画する
+		/// </summary>
+		/// <param name="vertices">矩形の情報</param>
+		/// <param name="texture">テクスチャの情報</param>
+		void Render(gamebasemaker::Vertices vertices, Texture texture);
+
+	private:
+		device::DirectXGraphicDevice* m_DXGraphicDevice;
+	};
+}
+
+#endif // !RENDERER_H_
