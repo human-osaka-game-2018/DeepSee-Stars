110233b5ea176de509ebad03562c15d61cad05d1
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 23:35:19 +0900

    タイトルの背景を描画するクラスの作成

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/TitleScene/BackGround/TitleBackGround.h
@@ -0,0 +1,45 @@
+﻿#ifndef TITLE_BACK_GROUND_H_
+#define TITLE_BACK_GROUND_H_
+
+#include "GameObject.h"
+
+namespace deepseestars
+{
+	/// <summary>
+	/// タイトルの背景クラス
+	/// </summary>
+	class TitleBackGround :public GameObject
+	{
+	public:
+		TitleBackGround() :
+			m_pFileName(_T("2DTexture/Title/TitleBack.jpg")), m_pTextureKey(_T("Back"))
+		{
+			Init();
+		}
+
+		~TitleBackGround() {}
+
+		void Init() 
+		{
+			m_rGameBaseMaker.CreateTex(m_pTextureKey, m_pFileName);
+		}
+
+		void Update() {}
+
+		void Render() 
+		{
+			m_rGameBaseMaker.Render(m_vertices, m_rGameBaseMaker.GetTex(m_pTextureKey));
+		}
+
+		void Release() 
+		{
+			m_rGameBaseMaker.ReleaseAllTex();
+		}
+
+	private:
+		const TCHAR* m_pFileName;
+		const TCHAR* m_pTextureKey;
+	};
+}
+
+#endif // !TITLE_BACK_GROUND_H_
