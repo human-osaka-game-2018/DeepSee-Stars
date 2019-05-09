009184eb226f0c379af8e6e1b046c8e010830f88
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 13:04:50 +0900

    画像を描画するクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Graphics/Renderer/Renderer.cpp
@@ -0,0 +1,45 @@
+﻿#include "Renderer.h"
+
+namespace graphics
+{
+	Renderer::Renderer()
+		:m_DXGraphicDevice(device::DirectXDevices::GetDXGraphicDevice())
+	{
+
+	}
+
+	void Renderer::PrepareRendering()
+	{
+		 m_DXGraphicDevice->GetGraphicDevice()->Clear(
+			0,
+			NULL,
+			D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
+			D3DCOLOR_XRGB(0, 0, 0),					//画面をクリアするときの色
+			1.f,
+			0);
+
+		 m_DXGraphicDevice->GetGraphicDevice()->BeginScene();
+	}
+
+	void Renderer::CleanUpRendering()
+	{
+		m_DXGraphicDevice->GetGraphicDevice()->EndScene();
+
+		m_DXGraphicDevice->GetGraphicDevice()->Present(NULL, NULL, NULL, NULL);
+	}
+
+	void Renderer::Render(gamebasemaker::Vertices vertices, Texture texture)
+	{
+		m_DXGraphicDevice->GetGraphicDevice()->SetFVF(
+			D3DFVF_XYZRHW |
+			D3DFVF_DIFFUSE |
+			D3DFVF_TEX1);
+
+		m_DXGraphicDevice->GetGraphicDevice()->SetTexture(0, texture.GetTex());
+
+		m_DXGraphicDevice->GetGraphicDevice()->DrawPrimitiveUP(
+			D3DPT_TRIANGLEFAN,
+			2,
+			vertices.Get(), sizeof(gamebasemaker::CustomVertex));
+	}
+}
