67b526046bfa48fd198ca27371ba48281096dc5f
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-09 13:31:32 +0900

    キーボードの入力を判断するクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Input/DirectXInput/Keyboard.cpp
@@ -0,0 +1,43 @@
+﻿#include "Keyboard.h"
+
+namespace input
+{
+	void Keyboard::CheckInputStateDetatils()
+	{
+		bool isInputtedPrev = false;
+		bool isInputted = false;
+
+		const int KEYBOARD_KEYS_MAX = 256;
+		ZeroMemory(m_details, sizeof(bool) * KEYBOARD_KEYS_MAX);
+		
+		for (int i = 0; i < KEYBOARD_KEYS_MAX; ++i)
+		{
+			isInputtedPrev = IsInputted(m_prevDiks[i]);
+			isInputted = IsInputted(m_diks[i]);
+
+			if (isInputtedPrev)
+			{
+				m_details[i] = (isInputted) ? IND_HOLD : IND_RELEASE;
+
+				continue;
+			}
+
+			m_details[i] = (isInputted) ? IND_PRESS : IND_NEUTRAL;
+		}
+	}
+
+	void Keyboard::Create()
+	{
+		LPDIRECTINPUT8 pInputDevice = device::DirectXDevices::GetDXInputDevice()->Get();
+
+		pInputDevice->CreateDevice(
+			GUID_SysKeyboard, &m_pDXInputDevice,
+			NULL);
+
+		m_pDXInputDevice->SetDataFormat(&c_dfDIKeyboard);
+
+		m_pDXInputDevice->SetCooperativeLevel(
+			m_HWND,
+			DISCL_NONEXCLUSIVE | DISCL_BACKGROUND);
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/GameBaseMaker/Input/DirectXInput/Keyboard.h
@@ -0,0 +1,111 @@
+﻿#ifndef KEYBOARD_H_
+#define KEYBOARD_H_
+
+#include "../../../Device/DirectXDevices/DirectXDevices.h"
+#include "../InputDevice/InputDevice.h"
+
+namespace input
+{
+	class Keyboard:public InputDevice
+	{
+	public:
+		Keyboard() 
+		{
+			Create();
+		}
+
+		~Keyboard() 
+		{
+			m_pDXInputDevice->Release();
+		}
+		
+		/// <summary>
+		/// キーボードのボタンの状態の更新
+		/// メインループの初めに呼ぶ
+		/// </summary>
+		inline void UpdateKeyState()
+		{
+			AcquireInputState();
+			CheckInputStateDetatils();
+		}
+
+		/// <summary>
+		/// キーボードのボタンの状態を保存する
+		/// </summary>
+		inline void PreserveKeyState()
+		{
+			memcpy(m_prevDiks, m_diks, sizeof(BYTE) * 256);
+		}
+
+		/// <summary>
+		/// ボタンが押された瞬間かを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>押された瞬間true</returns>
+		inline bool IsPressed(int key)
+		{
+			return (m_details[key] == IND_PRESS);
+		}
+
+		/// <summary>
+		/// ボタンが押されているかを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>押されていたらtrue</returns>
+		inline bool IsHold(int key)
+		{
+			return (m_details[key] == IND_HOLD);
+		}
+
+		/// <summary>
+		/// ボタンが離された瞬間かを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>離された瞬間true</returns>
+		inline bool IsReleased(int key)
+		{
+			return (m_details[key] == IND_RELEASE);
+		}
+
+		/// <summary>
+		/// ボタンが離されているかを返す
+		/// </summary>
+		/// <param name="key">ボタンのキー番号</param>
+		/// <returns>離されていたらtrue</returns>
+		inline bool IsNeutral(int key)
+		{
+			return (m_details[key] == IND_NEUTRAL);
+		}
+
+	private:
+		/// <summary>
+		/// デバイスの生成
+		/// </summary>
+		void Create();
+
+		/// <summary>
+		/// キーボードの状態を取得する権限と、キーボードの状態を取得する
+		/// </summary>
+		inline void AcquireInputState()
+		{
+			m_pDXInputDevice->Acquire();		
+
+			m_pDXInputDevice->GetDeviceState(
+				sizeof(BYTE) * 256,
+				m_diks);
+		}
+
+		/// <summary>
+		/// キーの状態を更新する
+		/// AcquireInputState()を読んだ後に用いる
+		/// </summary>
+		void CheckInputStateDetatils();
+
+		BYTE m_diks[256];
+		BYTE m_prevDiks[256];
+
+		INPUT_DETAIL m_details[256];
+	};
+}
+
+#endif // !KEYBOARD_H_
