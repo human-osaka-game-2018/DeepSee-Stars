dfcdbac2a522ab49e64ebebf6e76fde6998103b6
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-23 14:53:40 +0900

    同じところをぐるぐる回るクラスの作成

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemy/BaseEnemyAction/Patorl/Patrol.cpp
@@ -0,0 +1,102 @@
+﻿#include "Patrol.h"
+
+namespace deepseestars
+{
+	void Patrol::Init(bool isClockWise)
+	{
+		const int FOUR_WAYS = 4;
+		int pattern = static_cast<int>(isClockWise);
+
+		Direction nextDirection[2][FOUR_WAYS] = 
+		{
+			{ DOWN,UP,LEFT,RIGHT },
+			{ UP,DOWN,RIGHT,LEFT}
+		};
+
+		//向かう座標
+		D3DXVECTOR2 distinations[2][FOUR_WAYS] =
+		{
+			{
+				m_initPos,
+				m_movingRange,
+				{m_movingRange.x, m_initPos.y},
+				{m_initPos.x, m_movingRange.y}
+			},
+
+			{
+				{m_initPos.x, m_movingRange.y},
+				{m_movingRange.x, m_initPos.y},
+				m_initPos,
+				m_movingRange,
+			}
+		};
+
+		//移動する前の座標
+		D3DXVECTOR2 beginToMovePoint[2][FOUR_WAYS] =
+		{
+			{
+				{m_movingRange.x, m_initPos.y},
+				{m_initPos.x, m_movingRange.y},
+				 m_movingRange,
+				 m_initPos,
+			},
+
+			{
+				m_movingRange,
+				m_initPos,
+				{m_initPos.x, m_movingRange.y},
+				{m_movingRange.x, m_initPos.y},
+			}
+		};
+
+		for (int i = 0; i < FOUR_WAYS; ++i)
+		{
+			m_nextDirection[i] = nextDirection[pattern][i];
+			m_beginToMovePoint[i] = beginToMovePoint[pattern][i];
+			m_distinations[i] = distinations[pattern][i];
+		}
+	}
+
+	void Patrol::Update()
+	{
+		DesideRoute();
+		Move();
+	}
+
+	void Patrol::DesideRoute()
+	{
+		if (m_isMoving) return;
+
+		m_isMoving = true;
+
+		m_translationData.m_direction = m_nextDirection[m_translationData.m_direction];
+	}
+
+	void Patrol::Move()
+	{
+		if (!m_isMoving) return;
+
+		const float MOVE_SPEED_FRAME = 5.0f;
+
+		D3DXVECTOR2 distanceMax = 
+			m_distinations[m_translationData.m_direction] - m_beginToMovePoint[m_translationData.m_direction];
+
+		m_translationData.m_speed = distanceMax / MOVE_SPEED_FRAME;
+
+		m_translationData.m_movement = m_translationData.m_speed;
+
+		bool finishedMoving[DOWN + 1] =
+		{
+			m_translationData.m_pos.x <= m_distinations[m_translationData.m_direction].x,
+			m_translationData.m_pos.x >= m_distinations[m_translationData.m_direction].x,
+			m_translationData.m_pos.y <= m_distinations[m_translationData.m_direction].y,
+			m_translationData.m_pos.y >= m_distinations[m_translationData.m_direction].y
+		};
+
+		if (finishedMoving[m_translationData.m_direction])
+		{
+			m_isMoving = false;
+		}
+		else m_translationData.m_pos += m_translationData.m_movement;
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemy/BaseEnemyAction/Patorl/Patrol.h
@@ -0,0 +1,44 @@
+﻿#ifndef PATROL_H_
+#define PATROL_H_
+
+#include "../BaseEnemyAction.h"
+
+namespace deepseestars
+{
+	class Patrol :public BaseEnemyAction
+	{
+	public:
+		Patrol(TranslationData& translationData,const EnemyMovingData& movingData) :
+			BaseEnemyAction(translationData, movingData.m_initPos, movingData.m_movingPos)
+		{
+			Init(movingData.m_isClockWise);
+		}
+
+		~Patrol() {}
+
+		void Init(bool isClockWise);
+		void Update();
+
+	private:
+		/// <summary>
+		/// 巡回順路を決める
+		/// </summary>
+		void DesideRoute();
+
+		/// <summary>
+		/// 動かす
+		/// </summary>
+		void Move();
+
+		//向かう座標
+		D3DXVECTOR2 m_distinations[4];
+
+		//移動する前の座標
+		D3DXVECTOR2 m_beginToMovePoint[4];
+
+		//回る方向を決める
+		Direction m_nextDirection[4];
+	};
+}
+
+#endif // !PATROL_H_
