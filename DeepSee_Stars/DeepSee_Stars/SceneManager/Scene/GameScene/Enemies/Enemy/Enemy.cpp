4ccb4fe60d7020585a65d04f3cd564de25cd1ce4
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-23 14:52:51 +0900

    敵の動きのクラスの作成

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemy/Enemy.cpp
@@ -0,0 +1,72 @@
+﻿#include "Enemy.h"
+
+#include "BaseEnemyAction/Patorl/Patrol.h"
+
+namespace deepseestars
+{
+	Enemy::Enemy(const TCHAR* pTextureKey, const D3DXVECTOR2& playerPos,
+		Visibility visibility, EnemyMovingData movingData) :
+		GameObject(pTextureKey), m_playerPos(playerPos), m_visibility(visibility)
+	{
+		switch (movingData.m_actionPattern)
+		{
+		case PATROL:
+			m_pEnemyAction = new Patrol(m_translationData, movingData);
+			
+			break;
+
+		case L_SHAPED:
+			break;
+
+		default:
+			break;
+		}
+	}
+
+	void Enemy::Init()
+	{
+		
+	}
+
+	void Enemy::Update()
+	{
+		ChasePlayer();
+		DecideDirection();
+	}
+
+	void Enemy::Render()
+	{
+		m_vertices.SetPos(m_translationData.m_pos);
+	}
+
+	void Enemy::Release()
+	{
+		delete m_pEnemyAction;
+		m_pEnemyAction = nullptr;
+	}
+
+	void Enemy::DecideDirection()
+	{
+		if (m_translationData.m_movement == 0) return;
+
+		if (m_translationData.m_movement.x > 0) m_translationData.m_direction = RIGHT;
+		if (m_translationData.m_movement.x < 0) m_translationData.m_direction = LEFT;
+		if (m_translationData.m_movement.y < 0) m_translationData.m_direction = UP;
+		if (m_translationData.m_movement.y > 0) m_translationData.m_direction = DOWN;
+	}
+
+	void Enemy::ChasePlayer()
+	{
+		if (!m_existsPlayer) return;
+
+		D3DXVECTOR2 chaseVec = m_playerPos - m_translationData.m_pos;
+		D3DXVec2Normalize(&chaseVec, &chaseVec);
+
+		m_translationData.m_movement = 
+		{ chaseVec.x * m_translationData.m_speed.x,chaseVec.y * m_translationData.m_speed.y };
+
+		if (!m_canMove) return;
+
+		m_translationData.m_pos += m_translationData.m_movement;
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemy/Enemy.h
@@ -0,0 +1,68 @@
+﻿#ifndef ENEMY_H_
+#define ENEMY_H_
+
+#include "GameObject.h"
+#include "../../Enum/Direction.h"
+#include "BaseEnemyAction/BaseEnemyAction.h"
+
+namespace deepseestars
+{
+	/// <summary>
+	/// 視界の危険度
+	/// </summary>
+	enum Visibility
+	{
+		NORMAL,
+		ATTENTION,
+		DANGER
+	};
+
+	/// <summary>
+	/// 敵クラス
+	/// </summary>
+	class Enemy :public GameObject
+	{
+	public:
+		Enemy(const TCHAR* pTextureKey, const D3DXVECTOR2& playerPos,
+			Visibility visibility, EnemyMovingData movingData);
+
+		~Enemy()
+		{}
+
+		void Init();
+		void Update();
+		void Render();
+		void Release();
+
+		inline void SetExistsPlayer(bool existsPlayer) { m_existsPlayer = existsPlayer; }
+		inline void SetDirection(Direction direction)  { m_translationData.m_direction = direction; }
+		inline void SetCanMove(bool canMove)			{ m_canMove = canMove; }
+
+		inline Direction   GetDirection() { return m_translationData.m_direction; }
+		inline Visibility  GetVisibility() { return m_visibility; }
+		inline BaseEnemyAction::TranslationData GetTranslationData()	    { return m_translationData; }
+
+	private:
+		/// <summary>
+		/// プレイヤーを追いかける
+		/// </summary>
+		void ChasePlayer();
+
+		/// <summary>
+		/// 方向を決める
+		/// </summary>
+		void DecideDirection();
+
+		const D3DXVECTOR2& m_playerPos;
+
+		Visibility m_visibility;
+		BaseEnemyAction::TranslationData m_translationData;
+
+		BaseEnemyAction* m_pEnemyAction;
+
+		bool m_existsPlayer;
+		bool m_canMove;
+	};
+}
+
+#endif // !ENEMY_H_
