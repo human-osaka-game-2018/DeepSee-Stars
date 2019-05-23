8ce33923ed5a57382fcd420aad1df4cbf4a8121a
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-23 14:51:43 +0900

    敵の挙動をまとめたクラスの追加

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemies.cpp
@@ -0,0 +1,86 @@
+﻿#include "Enemies.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+namespace deepseestars
+{
+	void Enemies::Init()
+	{
+		for (auto enemy : m_enemies)
+		{
+			enemy->Init();
+		}
+	}
+
+	void Enemies::Load(const char* pFileName)
+	{
+		std::ifstream ifs(pFileName);
+		std::string str;
+
+		//1行目を無視する
+		std::getline(ifs, str);
+
+		const int XY = 2;
+
+		int enemyInitPos[XY];		//初期位置の設定xy(セルのマス数)
+		int enemyMovementPos[XY];	//移動する範囲の設定(セルのマス数)
+		int visibility;				//初期の視界レベル
+		int actionPattern;
+		EnemyMovingData movingData;
+
+		while (getline(ifs, str))
+		{
+			replace(str.begin(), str.end(), ',', ' ');
+			std::stringstream EnemiesStream(str);
+
+			//初期情報の読み込み
+			EnemiesStream >>
+				enemyInitPos[0] >> enemyInitPos[1] >>
+				enemyMovementPos[0] >> enemyMovementPos[1] >>
+				visibility >> actionPattern >> movingData.m_isClockWise;
+
+			movingData.m_initPos =
+			{ enemyInitPos[0] * m_cellSize,enemyInitPos[1] * m_cellSize };
+			
+			movingData.m_movingPos = 
+			{ enemyMovementPos[0] * m_cellSize,enemyMovementPos[1] * m_cellSize };
+
+			//敵の生成
+			m_enemies.push_back(new Enemy(m_pTextureKey, m_playerPos,
+				static_cast<Visibility>(visibility), movingData));
+		}
+	}
+
+	void Enemies::Update()
+	{
+		for (auto enemy : m_enemies)
+		{
+			enemy->Update();
+		}
+	}
+
+	void Enemies::Render()
+	{
+		for (auto enemy : m_enemies)
+		{
+			enemy->Render();
+		}
+	}
+
+	void Enemies::Release()
+	{
+		for (auto enemy : m_enemies)
+		{
+			enemy->Release();
+
+			delete enemy;
+			enemy = nullptr;
+		}
+
+		m_enemies.clear();
+		m_enemies.shrink_to_fit();
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemies.h
@@ -0,0 +1,39 @@
+﻿#ifndef ENEMIES_H_
+#define ENEMIES_H_
+
+#include <vector>
+
+#include "GameObject.h"
+#include "Enemy/Enemy.h"
+
+namespace deepseestars
+{
+	class Enemies :public GameObject
+	{
+	public:
+		Enemies(const D3DXVECTOR2& playerPos, const float& cellSize) :
+			GameObject(), m_playerPos(playerPos), m_cellSize(cellSize)
+		{}
+
+		~Enemies()
+		{
+		}
+
+		void Init();
+		void Update();
+		void Render();
+		void Release();
+
+		std::vector<Enemy*> GetEnemies() { return m_enemies; }
+
+	private:
+		void Load(const char* pFileName);
+
+		std::vector<Enemy*> m_enemies;
+
+		const D3DXVECTOR2& m_playerPos;
+		const float& m_cellSize;
+	};
+}
+
+#endif // !ENEMIES_H_
