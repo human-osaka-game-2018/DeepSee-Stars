7c6bf5d71ed302d64c78ee8a1ea63e676e283e09
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-24 18:52:09 +0900

    L字の点を移動するパターンのクラスを追加

---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemy/BaseEnemyAction/L_Shaped/L_Shaped.cpp
@@ -0,0 +1,24 @@
+﻿#include "L_Shaped.h"
+
+namespace deepseestars
+{
+	void L_Shaped::Init()
+	{
+		//0,2だけの乱数
+		int randam = 2 * (rand() % 2);
+
+		//最初の移動方向を決める
+		m_distance = m_dest[randam] - m_dest[1];
+
+		//リングバッファの設定
+		m_destQueue.push_back(randam);
+		m_destQueue.push_back(1);
+		m_destQueue.push_back(ReverseValue(0, 2, randam));	//選ばれなかったほうの値を詰める
+	}
+
+	void L_Shaped::Update()
+	{
+		ArriveAtDestination(m_dest);
+		UpdatePos();
+	}
+}
---
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemy/BaseEnemyAction/L_Shaped/L_Shaped.h
@@ -0,0 +1,48 @@
+﻿#ifndef L_SHAPED_H_
+#define L_SHAPED_H_
+
+#include "../BaseEnemyAction.h"
+
+namespace deepseestars
+{
+	/// <summary>
+	/// L字の点を巡回するクラス
+	/// </summary>
+	class L_Shaped :public BaseEnemyAction
+	{
+	public:
+		/// <summary>
+		/// 敵の動きの情報を受け渡すコンストラクタ
+		/// </summary>
+		/// <param name="translationData">敵の動きのデータ構造体</param>
+		/// <param name="movingData">初期位置や目的地のデータ構造体</param>
+		L_Shaped(TranslationData& translationData, const EnemyMovingData& movingData) :
+			BaseEnemyAction(translationData, movingData.m_initPos, movingData.m_movingPos)
+		{
+			Init();
+		}
+
+		~L_Shaped() {}
+
+		/// <summary>
+		/// デキューの初期化
+		/// </summary>
+		void Init();
+
+		/// <summary>
+		/// 更新関数
+		/// </summary>
+		void Update();
+
+	private:
+		//目的地の座標
+		D3DXVECTOR2 m_dest[3] =
+		{
+			{m_initPos.x,m_movingRange.y},
+			m_initPos,
+			{m_movingRange.x,m_initPos.y}
+		};
+	};
+}
+
+#endif // !L_SHAPED_H_
