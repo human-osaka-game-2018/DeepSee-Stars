ac49741013989a95352c81083811f1b5dde78b70
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2019-05-30 14:54:46 +0900

    敵の読み込みの時に使う情報を構造体に変更

--- DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemies.cpp
+++ DeepSee_Stars/DeepSee_Stars/SceneManager/Scene/GameScene/Enemies/Enemies.cpp
@@ -36,10 +36,15 @@ namespace deepseestars
 		//1行目を無視する
 		std::getline(ifs, str);
 
-		const int XY = 2;
+		struct Vector2Int
+		{
+		public:
+			int m_x;	
+			int m_y;
+		};
 
-		int enemyInitPos[XY];		//初期位置の設定xy(セルのマス数)
-		int enemyMovementPos[XY];	//移動する範囲の設定(セルのマス数)
+		Vector2Int enemyInitPos;		//初期位置の設定xy(セルのマス数)
+		Vector2Int enemyMovementPos;	//移動する範囲の設定(セルのマス数)
 		int visibility;				//初期の視界レベル
 		int actionPattern;
 		EnemyMovingData movingData;
@@ -51,15 +56,15 @@ namespace deepseestars
 
 			//初期情報の読み込み
 			EnemiesStream >>
-				enemyInitPos[0] >> enemyInitPos[1] >>
-				enemyMovementPos[0] >> enemyMovementPos[1] >>
+				enemyInitPos.m_x >> enemyInitPos.m_y >>
+				enemyMovementPos.m_x >> enemyMovementPos.m_y >>
 				visibility >> actionPattern >> movingData.m_isClockWise;
 
 			movingData.m_initPos =
-			{ enemyInitPos[0] * m_cellSize,enemyInitPos[1] * m_cellSize };
+			{ enemyInitPos.m_x * m_cellSize,enemyInitPos.m_y * m_cellSize };
 			
 			movingData.m_movingPos = 
-			{ enemyMovementPos[0] * m_cellSize,enemyMovementPos[1] * m_cellSize };
+			{ enemyMovementPos.m_x * m_cellSize,enemyMovementPos.m_y * m_cellSize };
 
 			movingData.m_actionPattern = static_cast<ActionPattern>(actionPattern);
 
